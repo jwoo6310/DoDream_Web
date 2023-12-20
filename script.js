0f0a34af7ae8626347bd29bf7c9079cbdce12d91
Jaeung Woo <142272074+jwoo6310@users.noreply.github.com>
2023-12-20 04:30:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // script.js
+/* Background Transition */
 document.addEventListener("DOMContentLoaded", function () {
   const backgroundElement = document.querySelector(".background-image");
   let currentIndex = parseInt(sessionStorage.getItem("backgroundIndex")) || 1;
@@ -18,3 +19,31 @@ document.addEventListener("DOMContentLoaded", function () {
   changeBackgroundImage(currentIndex);
 });
 
+/* Youtube */
+// Replace with your actual API Key
+var apiKey = 'AIzaSyDNVB-FtBheiZx_4zaY0tC31Nc9VWaGmM0';
+
+// Replace with the channel ID you want to fetch videos from
+var channelId = 'UC-YSiZ8LPKGur2MVHN95slQ';
+
+$(document).ready(function () {
+    // Fetch the latest video using YouTube Data API
+    $.get(
+        'https://www.googleapis.com/youtube/v3/search', {
+        part: 'snippet',
+        channelId: channelId,
+        maxResults: 1,
+        order: 'date',
+        type: 'video',
+        key: apiKey
+    },
+        function (data) {
+            if (data.items.length > 0) {
+                var videoId = data.items[0].id.videoId;
+                var embedUrl = 'https://www.youtube.com/embed/' + videoId;
+                var iframe = '<iframe width="560" height="315" src="' + embedUrl + '" frameborder="0" allowfullscreen></iframe>';
+                $('.youtube-video').html(iframe);
+            }
+        }
+    );
+});
